130c84daaaf8ad8a01c88a933e14624e8657bcba
kiranbandi <bvenkatkiran@gmail.com>
2019-07-19 13:38:56 -0600

    ability to pull data for all nodes in a model selectively for a given year

--- records/record.controller.js
+++ records/record.controller.js
@@ -4,6 +4,7 @@ const recordService = require('./record.service');
 
 // routes
 router.post('/flow-data', getRecords);
+router.post('/yearly-flow-data', getRecordsForYear);
 
 
 module.exports = router;
@@ -13,6 +14,16 @@ function getRecords(req, res, next) {
     let { modelID, threshold, number, type } = req.body;
 
     recordService.getRecords(modelID, threshold, number, type)
+        .then(records => res.json(records))
+        .catch(err => next(err));
+}
+
+
+function getRecordsForYear(req, res, next) {
+    //  this comes unwrapped from the JWT token
+    let { modelID, threshold, year } = req.body;
+
+    recordService.getRecordsForYear(modelID, threshold, year)
         .then(records => res.json(records))
         .catch(err => next(err));
 }
--- records/record.service.js
+++ records/record.service.js
@@ -2,9 +2,14 @@ const db = require('../helpers/db');
 const Record = db.Record;
 
 module.exports = {
-    getRecords
+    getRecords,
+    getRecordsForYear
 };
 
 async function getRecords(modelID, threshold, number, type) {
     return await Record.find({ modelID, threshold, number, type });
+}
+
+async function getRecordsForYear(modelID, threshold, year = '1990', type = 'demand') {
+    return await Record.find({ modelID, threshold, type, 'timestamp': { $regex: year, $options: 'i' } });
 }
